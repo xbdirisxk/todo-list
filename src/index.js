db3a0f8e3a2ccd2a948aec2d0bf19b3bbc552964
abdirisakhd <abdirisakjr@gmail.com>
2021-10-31 14:58:57 +0300

    add colors

---
+++ src/index.js
@@ -0,0 +1,1 @@
+// start globall code here
---
+++ webpack-config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = {
+	mode: "development",
+	entry: "./src/index.js",
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	model: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+		],
+	},
+};
